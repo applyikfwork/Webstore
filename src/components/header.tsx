b5131efeab17cbc2357384ced1d64f16de5ba451
Xyzapplywork <xyzapplywork@gmail.com>
2025-08-10 10:13:06 +0000

    If admin turn of login then don't show in home page to login button

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -72,7 +72,7 @@ export function Header() {
                         <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
                         <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
                     </Button>
-                     {authLoading ? (
+                     {authLoading || loading ? (
                        <Skeleton className="h-8 w-24" />
                     ) : user ? (
                         <>
